063de461f0d6e355e42a1f44807019d0a1d08046
Stephen <stephen.dawe94@gmail.com>
2018-09-30 22:13:45 -0400

    adding in the models

---
+++ models/article.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const articleSchema = new Schema ({
+
+    title: {
+        type: String,
+        required: true
+    },
+
+    link: {
+        type: String,
+        required: true
+    },
+
+    subTitle: {
+        type: String,
+        required: true
+    },
+
+    note: {
+        type: Schema.Types.ObjectId,
+        ref:"note"
+    }
+})
