2dd290ce107adf1d429e69a59f857b44e40e875a
Stephen <stephen.dawe94@gmail.com>
2018-10-01 12:13:44 -0400

    filled in API routes for GET and POST

--- controllers/scraper.js
+++ controllers/scraper.js
@@ -1,5 +1,7 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
+const db = require("../models");
+
 
 module.exports = function(app) {
 
@@ -28,6 +30,8 @@ axios.get("https://www.nytimes.com/").then(function(response) {
      *    The srcset value is used instead of src in this case because of how they're displaying the images
      *    Visit the website and inspect the DOM if there's any confusion
     */
+
+    
     var div = $(element).find("a").attr("href");
     var title = $(element).find("h2").text();
     let placeholder = $(element).find("p").text();
@@ -59,12 +63,53 @@ axios.get("https://www.nytimes.com/").then(function(response) {
 
     // Push the image's URL (saved to the div var) into the results array
     results.push({ link: div }, {text: title}, {subText: finalSub});
+
+    var entry = {}
+    entry.link = div;
+    entry.title = title;
+    entry.subTitle = finalSub;
+
+    db.article.create(entry).then(function(dbArticle){
+      console.log(dbArticle);
+    }).catch(function(err) {
+      return res.json(err);
+    })
+  });
+});
+
+app.get("/articles", function(req, res) {
+
+  db.article.find({}).then(function(dbArticle) {
+    res.json(dbArticle);
+  }).catch(function(err) {
+    res.json(err);
+  });
+});
+
+app.get("/articles/:id", function(req, res) {
+
+  db.article.findOne({_id: req.params.id})
+    .populate("note").then(function(dbArticle) {
+      res.json(dbArticle);
+    }).catch(function(err) {
+      res.json(err);
+    });
   });
 
-  // After looping through each element found, log the results to the console
-  console.log(results);
+app.post("/articles/:id", function(req, res) {
+
+  db.note.create(req.body).then(function(dbNote) {
+
+    return db.article.findOneAndUpdate({_id: req.params.id}, {note: dbNote._id}, {new: true});
+  }).then(function(dbArticle){
+    res.json(dbArticle);
+  }).catch(function(err) {
+    res.json(err);
+  });
 });
-}
+
+
+};
 
 
 
